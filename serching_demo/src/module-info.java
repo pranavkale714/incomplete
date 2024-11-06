89fca00a3b8b33c86c74149c47f1caf6507c923f
pranavkale714 <96164632+pranavkale714@users.noreply.github.com>
2024-11-07 00:25:13 +0530

    Add files via upload

---
+++ serching_demo/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module serching_demo {
+}
---
+++ serching_demo/src/serching_demo/interval_search.java
@@ -0,0 +1,10 @@
+package serching_demo;
+
+public class interval_search {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+dcfvgf
+	}
+
+}
